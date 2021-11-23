2d5e169e61bc41948911b252694352adede7ace7
KFortySix <kleerivera@outlook.com>
2021-11-23 14:23:23 -0300

    Primer commit neodiagnos

---
+++ db/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "ec2-44-195-201-3.compute-1.amazonaws.com",
+    USER: "zgpufoqmpouibn",
+    PASSWORD: "ccdb0451d8eff038e64cb29e6b3d5e0bad696c78fee85a937b6807b6f7716c0f",
+    DB: "de3gtrfnhutkf9",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
