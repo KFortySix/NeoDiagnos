3e9e65b6782a702f3a4d690431b74b950bd3863e
KFortySix <kleerivera@outlook.com>
2021-11-23 16:14:32 -0300

    modificado db

--- db/config.js
+++ db/config.js
@@ -1,13 +1,12 @@
-module.exports = {
+import { Pool } from 'pg';
+
+const pool = new Pool({
     HOST: "ec2-44-195-201-3.compute-1.amazonaws.com",
     USER: "zgpufoqmpouibn",
     PASSWORD: "ccdb0451d8eff038e64cb29e6b3d5e0bad696c78fee85a937b6807b6f7716c0f",
-    DB: "de3gtrfnhutkf9",
-    dialect: "postgres",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
+    PORT: "5432",
+    database: "de3gtrfnhutkf9",
+    dialect: "postgres"
+});
+
+module.exports = pool;
